f5f0eee5a116c938bbc5d7a4b76a438cd8f8a6ff
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-09-17 12:53:35 -0400

    holaaa

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          holaaaaa
         </p>
       </div>
       <p className="read-the-docs">
